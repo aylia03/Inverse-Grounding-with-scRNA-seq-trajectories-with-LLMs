c1b36562ea98f6b01b60cfe6cbb3531c5c2ab128
aylia03 <aylia.mayer@gmail.com>
2026-04-02 15:08:16 +0200

    changed small details

--- src/llms_clients.py
+++ src/llms_clients.py
@@ -55,18 +55,18 @@ def call_Mistral(system_prompt, user_prompt):
 
 
 #### for cloud apis
-client = OpenAI(api_key=os.environ.get('DEEPSEEK_API_KEY'), base_url="https://api.deepseek.com")
+# client = OpenAI(api_key=os.environ.get('DEEPSEEK_API_KEY'), base_url="https://api.deepseek.com")
+#
+# response = client.chat.completions.create(
+#     model="deepseek-chat",
+#     messages=[
+#         {"role": "system", "content": "You are a helpful assistant"},
+#         {"role": "user", "content": "Hello"},
+#     ],
+#     stream=False
+# )
 
-response = client.chat.completions.create(
-    model="deepseek-chat",
-    messages=[
-        {"role": "system", "content": "You are a helpful assistant"},
-        {"role": "user", "content": "Hello"},
-    ],
-    stream=False
-)
-
-print(response.choices[0].message.content)
+# print(response.choices[0].message.content)
 ########################
 
 def call_llms(model, system_prompt, user_prompt):
