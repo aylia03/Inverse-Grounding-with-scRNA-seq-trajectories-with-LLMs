777657910d245a595766b588bcec7d908fceb3e4
aylia03 <aylia.mayer@gmail.com>
2026-04-09 16:05:06 +0200

    reworked CoT and CoT+RAG

--- src/prompts.py
+++ src/prompts.py
@@ -11,10 +11,10 @@ USER_PROMPT_2 = ("Given the following expression data of T-Cells from a chronic 
                  " whether this cell becomes exhausted or a not-exhausted t-cell. \n\n"
                  "Expression data: \n{json_input}\n\n"
                  "Let's think step by step: \n "
-                 "1. What does the expression pattern suggest about the current cell state? \n"
-                 "2. How does the value of the latent_time variable refelect the current position in the development of the "
-                 "cell? \n"
-                 "3. Now include also the insights from the given phase and timepoint of the cell data? \n"
+                 "1. What does the phase, timepoint and cluster indicate about the current cell state? \n"
+                 "2. How does the value of the latent_time and pseudotime refelect the current position in the development of the "
+                 "cell? Also the insights from the palantir PST. \n"
+                 "3. Analyze the top-10 expressed gene marker, what do they indicate about the current cell state? \n"
                  "4. Based on these observations, what is the most likely fate of the cell?\n\n" +
                 ANSWER_FORMAT)
 
@@ -23,8 +23,9 @@ USER_PROMPT_3 = ("Given the following expression data of T-Cells from a chronic 
                  "Expression data: \n{json_input}\n\n"
                  "Literature: \n{rag_context}\n\n"
                  "Let's think step by step: \n "
-                 "1. What exhaustion-associated genes from the literature are elevated or lowered in the current cell? \n"
-                 "2. Which non-exhaustion-associated genes from the literature are elevated in the current cell? \n"
-                 "3. What does the latent_time, phase, timepoint indicated about the cell\n"
+                 "1. What does the phase, timepoint, pseudotime, latent_time, Palantir PST and cluster indicate about "
+                 "the current position and trajectory of the cell? \n"
+                 "2. What exhaustion-associated genes from the literature are elevated or lowered in the current cell? \n"
+                 "3. Which non-exhaustion-associated genes from the literature are elevated in the current cell? \n"
                  "4. Based on the expression file and literature insights, what is the most likely fate of the cell?\n\n" +
                 ANSWER_FORMAT)
