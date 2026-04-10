641f80cf1767a08976b8aaca499b8f3f73352397
aylia03 <aylia.mayer@gmail.com>
2026-04-10 14:21:44 +0200

    added some testing

--- src/benchmark.py
+++ src/benchmark.py
@@ -12,7 +12,7 @@ with open("../Data/processed/labels_gt_data.json", "r") as f:
     gt = json.load(f)
 print("Groundtruth is loaded.\n")
 
-models = ["deepseek", "mistral"]
+models = ["deepseek"]
 prompts = {
     1: prompt.USER_PROMPT_1,
     2: prompt.USER_PROMPT_2,
@@ -36,11 +36,12 @@ for phase in range(2):  # geht gerade nur bis phase 2, weil phase 3 bruacht noch
     print("Phase:", phase+1) # iterate over phase
     for llm in models: # go over each llm
         preds = []  ## store predictions for each model
-        for cell in cells_data:
+        for cell in cells_data[:10]:
             if phase == 3:
-                user_prompt = prompts[phase].format(json_input = json.dumps(cell), rag_context = rag_data)
+                user_prompt = prompts[phase+1].format(json_input = json.dumps(cell), rag_context = rag_data)
             else:
-                user_prompt = prompts[phase].format(json_input = json.dumps(cell))
+                user_prompt = prompts[phase+1].format(json_input = json.dumps(cell))
 
             preds.append(parse_answer(call_llms(llm, prompt.SYSTEM_PROMPT, user_prompt)))
 
+        print(f"{llm} Phase {phase+1}: {preds}")
