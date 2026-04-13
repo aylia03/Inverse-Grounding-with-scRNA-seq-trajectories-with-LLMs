90678fde3ad3e2d803ab046013f8c166598307b4
aylia03 <aylia.mayer@gmail.com>
2026-04-13 13:28:15 +0200

    rag integration

--- src/benchmark.py
+++ src/benchmark.py
@@ -8,6 +8,8 @@ from llms_clients import call_llms
 
 from sklearn.metrics import f1_score
 
+import rag
+
 print("Loading the data ...")
 with open("../Data/processed/cells_data.json", "r") as f:
     cells_data = json.load(f)
@@ -23,7 +25,7 @@ prompts = {
     2: prompt.USER_PROMPT_2,
     3: prompt.USER_PROMPT_3
 }
-rag_data = ""
+rag_data = rag.get_rag_context()
 
 gt_labels = [x["label"] for x in gt]
 
@@ -41,12 +43,12 @@ def parse_answer(answer):
         return "exhausted"
     else: return "unknown"
 
-for phase in range(2):  # geht gerade nur bis phase 2, weil phase 3 bruacht noch das pubmed
+for phase in range(3):  # geht gerade nur bis phase 2, weil phase 3 bruacht noch das pubmed
     print("Phase:", phase+1) # iterate over phase
     for llm in models: # go over each llm
         preds = []  ## store predictions for each model
         for cell_idx, cell in enumerate(cells_data):
-            if phase == 3:
+            if phase == 2:
                 user_prompt = prompts[phase+1].format(json_input = json.dumps(cell), rag_context = rag_data)
             else:
                 user_prompt = prompts[phase+1].format(json_input = json.dumps(cell))
@@ -73,6 +75,6 @@ for phase in range(2):  # geht gerade nur bis phase 2, weil phase 3 bruacht noch
 df = pd.DataFrame(result)
 
 os.makedirs("../Data/results/", exist_ok=True)
-df.to_csv("../Data/results/benchmarking_res.csv", index=False )
+df.to_csv("../Data/results/benchmarking_res_test.csv", index=False )
 
 
